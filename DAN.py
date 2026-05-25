3e419a49be91cc8e15222cf159df43cbf60ec482
Logan <126506217+AlotarioPersonal@users.noreply.github.com>
2026-05-24 19:17:28 -0500

    added secondary DAN, to load system plugins individually

---
+++ DAN.py
@@ -0,0 +1,30 @@
+'''
+
+   D.A.N (Docked protocol ApplicatioN)
+
+   Version 0.1, 2026-5-22
+
+   Secondary silent partner system to URIA, focused on numpad input commands over claps. Used to activate specific plugins by number, modulation the plugin system of URIA to individual scripts.
+   Whereas URIA will load a set list of plugins automatically in one big giant list, DAN will load individual plugins in the order you punch them in.
+   
+
+   REQUIRED PACKAGES:
+
+'''
+
+print("""D.A.N SYSTEM""")
+
+import os
+commandstr = ""
+
+
+while True:
+	try:
+		xrequire = input("DAN ~$:")
+		if xrequire=="0":
+			exit()
+		else:
+			commandstr = ("python plugins/" + str(xrequire) + ".py")
+			os.system(f"x-terminal-emulator -e '{str(commandstr)}'")
+	finally:
+		print('TASK COMPLETE')
