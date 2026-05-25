4f77a6e9d8b238e5906eebfcea1c51f81677def4
Logan <126506217+AlotarioPersonal@users.noreply.github.com>
2026-05-24 19:21:18 -0500

    i love commits

--- DAMN.py
+++ DAMN.py
@@ -5,14 +5,14 @@
    Version 0.1, 2026-5-24
 
    Secondary silent partner system to URIA, focused on numpad input commands over claps. Used to activate specific plugins by number, modulation the plugin system of URIA to individual scripts.
-   Whereas URIA will load a set list of plugins automatically in one big giant list, DAN will load individual plugins in the order you punch them in.
+   Whereas URIA will load a set list of plugins automatically in one big giant list, DAMN will load individual plugins in the order you punch them in.
    
 
    REQUIRED PACKAGES:
 
 '''
 
-print("""D.A.N SYSTEM""")
+print("""D.A.M.N SYSTEM""")
 
 import os
 commandstr = ""
@@ -20,7 +20,7 @@ commandstr = ""
 
 while True:
 	try:
-		xrequire = input("DAN ~$:")
+		xrequire = input("DAMN ~$:")
 		if xrequire=="0":
 			exit()
 		else:
